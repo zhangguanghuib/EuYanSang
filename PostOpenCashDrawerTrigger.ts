15403d0b8b6ff06b6d8ba22d2a755c171c9bf514
zhangguanghuib <zhangguanghuib@126.com>
2021-02-19 23:08:16 -0800

    Add files via upload

---
+++ PostOpenCashDrawerTrigger.ts
@@ -0,0 +1,10 @@
+﻿import * as Triggers from "PosApi/Extend/Triggers/PeripheralTriggers"
+import { ObjectExtensions } from "PosApi/TypeExtensions";
+import { ClientEntities, ProxyEntities } from "PosApi/Entities";
+
+export default class PostOpenCashDrawerTrigger extends Triggers.PostOpenCashDrawerTrigger {
+
+    public execute(options: Triggers.IPostOpenCashDrawerTriggerOptions): Promise<void> {
+        return Promise.resolve();
+    }
+}
---
+++ PrePaymentTrigger.ts
@@ -0,0 +1,60 @@
+﻿import * as Triggers from "PosApi/Extend/Triggers/PaymentTriggers";
+import { ObjectExtensions } from "PosApi/TypeExtensions";
+import { ClientEntities, ProxyEntities } from "PosApi/Entities";
+import { PrinterPrintRequest, PrinterPrintResponse } from "PosApi/Consume/Peripherals";
+import { GetHardwareProfileClientRequest, GetHardwareProfileClientResponse } from "PosApi/Consume/Device";
+import { GetReceiptsClientRequest, GetReceiptsClientResponse } from "PosApi/Consume/SalesOrders";
+
+export default class PrePaymentTrigger extends Triggers.PrePaymentTrigger {
+
+    public execute(options: Triggers.IPrePaymentTriggerOptions): Promise<ClientEntities.ICancelable> {
+        if (options.tenderType.TenderTypeId === '1') {
+            this.context.logger.logVerbose("Executing PostSuspendTransactionTrigger with options " + JSON.stringify(options) + ".");
+
+            if (ObjectExtensions.isNullOrUndefined(options) || ObjectExtensions.isNullOrUndefined(options.cart)) {
+                // This will never happen, but is included to demonstrate how to return a rejected promise when validation fails.
+                let error: ClientEntities.ExtensionError
+                    = new ClientEntities.ExtensionError("The options provided to the PostSuspendTransactionTrigger were invalid.");
+                return Promise.reject(error);
+            } else {
+                return this.context.runtime.executeAsync(new GetHardwareProfileClientRequest())
+                    .then((response: ClientEntities.ICancelableDataResult<GetHardwareProfileClientResponse>)
+                        : Promise<ClientEntities.ICancelableDataResult<GetReceiptsClientResponse>> => {
+                        let hardwareProfile: ProxyEntities.HardwareProfile = response.data.result;
+
+                        // Gets the receipts.
+                        let salesOrderId: string = options.cart.Id;
+                        let receiptRetrievalCriteria: ProxyEntities.ReceiptRetrievalCriteria = {
+                            IsCopy: false,
+                            IsRemoteTransaction: false,
+                            IsPreview: false,
+                            QueryBySalesId: false,
+                            ReceiptTypeValue: ProxyEntities.ReceiptType.Unknown,
+                            HardwareProfileId: hardwareProfile.ProfileId
+                        };
+                        let getReceiptsClientRequest: GetReceiptsClientRequest<GetReceiptsClientResponse> =
+                            new GetReceiptsClientRequest(salesOrderId, receiptRetrievalCriteria);
+
+                        return this.context.runtime.executeAsync(getReceiptsClientRequest);
+                    })
+                    .then((response: ClientEntities.ICancelableDataResult<GetReceiptsClientResponse>)
+                        : Promise<ClientEntities.ICancelableDataResult<PrinterPrintResponse>> => {
+                        let receipts: ProxyEntities.Receipt[] = response.data.result;
+
+                        // Prints the receipts.
+                        let printerPrintRequest: PrinterPrintRequest<PrinterPrintResponse> = new PrinterPrintRequest(receipts);
+
+                        return this.context.runtime.executeAsync(printerPrintRequest);
+                     }).then((): Promise<ClientEntities.ICancelable> => {
+                        // Resolves to a void result when fulfilled.
+                         return Promise.resolve({ canceled: false });
+                     }).catch((reason: any): Promise<ClientEntities.ICancelable> => {
+                        // Resolves to a void result when rejected. This matches existing POS printing behavior.
+                         this.context.logger.logError("PostSuspendTransactionTrigger execute error: " + JSON.stringify(reason));
+                         return Promise.resolve({ canceled: false });
+                    });
+            }
+        }
+        return Promise.resolve({ canceled: false });
+    }
+}
